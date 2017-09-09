c131a36c03c29ed484439bddb420458cdec0b4a0
Torara1998 <minh.dang160205@vnuk.edu.vn>
2017-09-09 17:20:09 +0700

    ex

---
+++ Letter.java
@@ -0,0 +1,17 @@
+public class Letter
+{
+	public static void main( String[] args )
+	{
+		System.out.println("+-----------------------------------------------+");
+		System.out.println("+                                           ####+");
+		System.out.println("+                                           ####+");
+		System.out.println("+                                           ####+");
+		System.out.println("+                                               +");
+		System.out.println("+                                               +");
+		System.out.println("+ 			DANG HIEU MINH                 +");
+		System.out.println("+			54/9 NGUYEN LU		      	   +");
+		System.out.println("+ 			QUY NHON, BINH DINH.           +");
+		System.out.println("+                                               +");
+		System.out.println("+-----------------------------------------------+");
+	}
+}	
---
+++ Variables.java
@@ -0,0 +1,17 @@
+public class Variables
+{
+	public static void main ( String [] args )
+	{
+		int A;
+		double B;
+		String C;
+		
+		A = 113;
+		B = 2.71828;
+		C = "Computer Science";
+		
+		System.out.println("This is room #" + A  );
+		System.out.println("e is close to " + B );
+		System.out.println("I am learing a bit about " + C );
+	}
+}
